f82473b1ba62bb2d86216c9e529d27ea31c305f5
Rohit3202 <rohitwaghmode56@gmail.com>
2024-12-01 18:13:53 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 1208  //unchanged
+let accountEmail = "rohit@gmail.com"
+var accountPassword = "12345"
+accountCity = "Rahuri"
+let accountState;  //undifined
+
+// accountId = 2   // Assignment to constant variable.
+
+accountEmail = "roh@gmail.com";
+accountPassword = "256";
+accountCity = "aahilyanagar";
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountState,accountCity]);
