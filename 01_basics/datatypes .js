34dc84252af1c941dd93ceab450f6a24736e5d64
Rohit3202 <rohitwaghmode56@gmail.com>
2024-12-01 18:21:32 +0000

    datatypes

---
+++ 01_basics/datatypes .js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello"); //error becs we are using nodejs not browser
+
+// console.log(3+4) console.log("hello") error 
+// console.log(3+4); console.log("hello") error solve ; is end of line
+
+//code redability should be high
+
+//premative datatypes
+//number    => 2 to power 53
+//bigint    => to store lagre number i will not use becz it is use to big wesite to store like facebook
+//boolean   => true/false
+//null      => stand alone value //*type object */
+//undefined => //do not assign
+//symbol    => find uniquness /unique
+
+
+//object
