fd67e89a8c083ea6577e4901469cc9520ba47f30
Rohit3202 <rohitwaghmode56@gmail.com>
2024-12-03 18:02:05 +0000

    datatypes

---
+++ 01_basics/04_comparisionDatatypes.js
@@ -0,0 +1,23 @@
+//Comparision of datatypes in js
+
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+//  console.log(2!=1); //return true or false
+
+// console.log("2">1);   // t
+// console.log("02">1); //true  convert automaticaly
+
+//av console.log(null>0);
+//av  console.log(null==0);
+//av console.log(null<=0); //convert valuses problem
+
+//av console.log(undefined==0);
+//av console.log(undefined>0);
+//av console.log(undefined<0); //avoid this
+
+// === strictly check number or its datatype
+
+// console.log("2"===2); //f
